dc42eb1a7a0a90a9090a17fb1e1b36ce3e218790
hutingrong <foxhutingrong@163.com>
2019-06-16 12:17:02 +0800

    springboot基本框架

---
+++ src/main/java/com/lala/annotation/MybatisAnnotation.java
@@ -0,0 +1,4 @@
+package com.lala.annotation;
+
+public @interface MybatisAnnotation {
+}
--- src/main/java/com/lala/dao/EmpMapper.java
+++ src/main/java/com/lala/dao/EmpMapper.java
@@ -1,29 +1,25 @@
 package com.lala.dao;
 
+import com.lala.annotation.MybatisAnnotation;
 import com.lala.dto.Emp;
 import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
-@Mapper
-public interface EmpMapper {
-    @Select("select * from emp")
+@Repository("empMapper")
+public @MybatisAnnotation interface EmpMapper {
+
     public List<Emp> findAllEmp();
 
-    @Insert("insert into emp(name,sex,job,salary,hiredate,deptno) values(#{name},#{sex},#{job},#{salary},#{hiredate},#{deptno})")
-    public void addEmp(@Param("name") String name, @Param("sex") String sex, @Param("job") String job,
-                       @Param("salary") double salary,@Param("hiredate") Date hiredate,@Param("deptno") int deptno);
+    public void addEmp(Emp emp);
 
-    @Delete("delete from emp where id=#{id}")
-    public void deleteEmp(@Param("id") int id);
+    public void deleteEmp(int id);
 
-    @Update("update emp set name =#{name},sex=#{sex},job=#{job},salary=#{salary},hiredate=#{hiredate},deptno=#{deptno} where id=#{id}")
-    public void updateEmp(@Param("id") int id,@Param("name") String name,@Param("sex") String sex,@Param("job") String job,
-                        @Param("salary") double salary,@Param("hiredate") Date hiredate,@Param("deptno") int deptno);
+    public void updateEmp(Emp emp);
 
-    @Select("select * from emp where id=#{id}")
-    public Emp findEmpById(@Param("id") int id);
+    public Emp findEmpById(int id);
 }
 
 
